9bb16e5c070f1e73f35f65e5bf96a2dba1bbce2d
MASINA Elliot <elliot.masina@epitech.eu>
2024-04-10 15:34:24 +0200

    [FEAT]: client connect to server without cmd managementhout

---
+++ Client/src/client.c
@@ -0,0 +1,61 @@
+/*
+** EPITECH PROJECT, 2024
+** myteams | client
+** File description:
+** client.c
+*/
+
+#include <string.h>
+
+#include "client.h"
+
+static int handle_input(int file_descriptor)
+{
+    char buffer[MAX_CHAR_SIZE] = {0};
+    char request[MAX_CHAR_SIZE] = {0};
+    int read_value = {0};
+
+    fgets(request, MAX_CHAR_SIZE, stdin);
+    request[strcspn(request, "\n")] = '\0';
+    if (write(file_descriptor, request, strlen(request)) < 0) {
+        perror("write");
+        return ERROR;
+    }
+    read_value = read(file_descriptor, buffer, MAX_CHAR_SIZE);
+    if (read_value < 0) {
+        perror("read");
+        return ERROR;
+    } else if (read_value == 0) {
+        return SUCCESS;
+    }
+    buffer[read_value] = '\0';
+    write(1, buffer, read_value);
+    return SUCCESS;
+}
+
+static int client_loop(struct client *client)
+{
+    while (1) {
+        if (handle_input(client->sockfd) == ERROR) {
+            return ERROR;
+        }
+    }
+    return SUCCESS;
+}
+
+int run_client(struct client *client)
+{
+    client->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client->sockfd < 0) {
+        perror("socket");
+        return ERROR;
+    }
+    client->socket_address.sin_family = AF_INET;
+    client->socket_address.sin_port = client->port;
+    if (connect(client->sockfd, (struct sockaddr *)&client->socket_address,
+                sizeof(client->socket_address)) < 0) {
+        perror("connect");
+        return ERROR;
+    }
+    return client_loop(client);
+}
---
+++ Client/src/parse_args.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2024
+** myteams | client
+** File description:
+** main.c
+*/
+
+#include <arpa/inet.h>
+#include <string.h>
+
+#include "client.h"
+
+int parse_args(const char *ip, const char *port, struct client *client)
+{
+    int inet_pton_return = 0;
+    int converted_port = atoi(port);
+
+    if (converted_port <= 0 || converted_port > 65535) {
+        write(2, "Port must be between 0 and 65535\n", 34);
+        return ERROR;
+    }
+    client->port = htons(converted_port);
+    inet_pton_return =
+        inet_pton(AF_INET, ip, &client->socket_address.sin_addr);
+    if (inet_pton_return <= 0) {
+        perror("inet_pton");
+        return ERROR;
+    } else {
+        client->ip = strdup(ip);
+        return SUCCESS;
+    }
+}
